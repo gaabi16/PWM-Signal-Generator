06a492fbfdf79f199872b0694214ec2c1e09e76a
Gabriel Crisan <gabicrisan01@gmail.com>
2025-12-14 21:15:46 +0200

    Fix variables unaligned identitation

--- code/pwm_gen.v
+++ code/pwm_gen.v
@@ -4,11 +4,11 @@ module pwm_gen (
     input rst_n,
     // PWM signal register configuration
     input pwm_en,
-    input [15:0] period,
-    input [7:0] functions,
-    input [15:0] compare1,
-    input [15:0] compare2,
-    input [15:0] count_val,
+    input[15:0] period,
+    input[7:0] functions,
+    input[15:0] compare1,
+    input[15:0] compare2,
+    input[15:0] count_val,
     // top facing signals
     output reg pwm_out
 );
